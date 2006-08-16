b021c538f5c35f6cfa8c677a202994d3cf91f8e2
Pavel Podgorny <pavel@podgorny.cz>
2006-08-16 14:15:55 +0200

    Logout changed to button

--- auth.php
+++ auth.php
@@ -6,23 +6,27 @@ session_start();
 function auth_header() {
 	if (auth()) {
 		echo "<div class=\"menu\">\n";
-	        echo "<ul><li><a href=\"admin.php\">Návrat do menu</a></li></ul>\n";
+			echo "<ul><li><a href=\"admin.php\">Návrat do menu</a></li></ul>\n";
 		echo "</div>\n";
 		echo "<div class=\"authorization\">\n";
-	        echo "<p class=\"login\"><a href=\"logout.php\">Odhlásit se</a></p>\n";
+		echo "<form action=\"logout.php\" method=\"post\">\n";
+		echo "<p>";
+			echo "<input type=\"submit\" value=\"Odhlásit se\" />";
+		echo "</p>\n";
+			echo "</form>\n";
 		echo "</div>\n";
 	} else {
 		echo "<div class=\"menu\">\n";
-	        echo "<ul><li><a href=\"admin.php\">Návrat do menu</a></li></ul>\n";
+			echo "<ul><li><a href=\"admin.php\">Návrat do menu</a></li></ul>\n";
 		echo "</div>\n";
 		echo "<div class=\"authorization\">\n";
-	        echo "<form action=\"login.php\" method=\"post\">\n";
+			echo "<form action=\"login.php\" method=\"post\">\n";
 		echo "<p>";
-	        echo "Heslo: ";
-	        echo "<input type=\"password\" name=\"password\" />";
-	        echo "<input type=\"submit\" value=\"Přihlásit se\" />";
+			echo "Heslo: ";
+			echo "<input type=\"password\" name=\"password\" />";
+			echo "<input type=\"submit\" value=\"Přihlásit se\" />";
 		echo "</p>\n";
-	        echo "</form>\n";
+			echo "</form>\n";
 		echo "</div>\n";
 	}
 }
