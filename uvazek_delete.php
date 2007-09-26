8fafe7df698bb80ca70708def3363f82c089467c
Radek Podgorny <radek@podgorny.cz>
2007-09-26 09:31:26 +0200

    Added "uvazky" facility

---
+++ uvazek_delete.php
@@ -0,0 +1,20 @@
+<?php require_once("sql.php"); ?>
+
+<?php
+
+$res = mysql_query("DELETE FROM uvazky WHERE id='" . $_GET["id"] . "';");
+
+?>
+
+<?php include("header.htm"); ?>
+
+<?php
+
+if ($res)
+	echo "V pořádku.";
+else
+	echo "CHYBA!!!";
+
+?>
+
+<?php include("footer.htm"); ?>
