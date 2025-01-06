391878c9f499d6327a035aa052fcdcbd934abd47
Radek Podgorny <radek@podgorny.cz>
2025-01-06 14:05:52 +0100

    update diety for 2025

--- src/totals.php
+++ src/totals.php
@@ -83,7 +83,9 @@ function day_totals($year, $month, $day, $employee) {
 			if ($at_cesta_mimopraha) {
 				$total["odpracovano"] += $secs - $secs_old;
 				if ($secs - $secs_old > 18*60*60) {
-					if ($year >= 2024) {
+					if ($year >= 2025) {
+						$total["diety_kc"] = 360;
+					} elseif ($year >= 2024) {
 						$total["diety_kc"] = 340;
 					} elseif ($year >= 2023) {
 						$total["diety_kc"] = 310;
@@ -113,7 +115,9 @@ function day_totals($year, $month, $day, $employee) {
 						$total["diety_kc"] = 150;
 					}
 				} else if ($secs - $secs_old > 12*60*60) {
-					if ($year >= 2024) {
+					if ($year >= 2025) {
+						$total["diety_kc"] = 230;
+					} elseif ($year >= 2024) {
 						$total["diety_kc"] = 220;
 					} elseif ($year >= 2023) {
 						$total["diety_kc"] = 200;
@@ -143,7 +147,9 @@ function day_totals($year, $month, $day, $employee) {
 						$total["diety_kc"] = 96;
 					}
 				} else if ($secs - $secs_old > 5*60*60) {
-					if ($year >= 2024) {
+					if ($year >= 2025) {
+						$total["diety_kc"] = 150;
+					} elseif ($year >= 2024) {
 						$total["diety_kc"] = 140;
 					} elseif ($year >= 2023) {
 						$total["diety_kc"] = 130;
