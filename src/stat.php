8c2613ded416e0bb4e092da61bee687b5f649cf8
Radek Podgorny <radek@podgorny.cz>
2026-01-03 12:35:58 +0100

    make plusminus printout more obvious

--- src/stat.php
+++ src/stat.php
@@ -94,7 +94,11 @@ $dovolena_zminula = db_get_condition("dovolene", "days_lastyear", "employee=".$_
 	</tr>
 	<tr>
 		<th>+/- tento měsíc</th>
-		<td><?php echo secs_to_time($cur_tots["plusminus"] - $cur_tots["overtime"]); ?></td>
+		<td><?php echo secs_to_time($cur_tots["plusminus"]); ?></td>
+	</tr>
+	<tr>
+		<th>+/- přesčasy</th>
+		<td>- <?php echo secs_to_time($cur_tots["overtime"]); ?></td>
 	</tr>
 	<tr>
 		<th>+/- celkem</th>
